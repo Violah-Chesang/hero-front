3da83e5635ff9e243d1608de67d06ce6b635a469
Violah Chesang <chesangv21@gmail.com>
2023-06-23 19:57:49 +0300

    Created the header for the Logo and Navbar

---
+++ src/components/header/header.js
@@ -0,0 +1,21 @@
+import React from "react";
+function Header(){
+    return (
+        <div className="container bg-gradient text-center">
+        <div className="row">
+            <div className="col-6">
+                <h1>Hero Squad</h1>
+
+            </div>
+            <div className="col-6">
+                <p>Navigation Bar Goes Here</p>
+
+
+            </div>
+        </div>
+        </div>
+    )
+};
+
+
+export default Header;
