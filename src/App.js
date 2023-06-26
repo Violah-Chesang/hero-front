c809424f6fc7e94fea71e2c0361074e30bde3a25
Violah Chesang <chesangv21@gmail.com>
2023-06-26 12:48:55 +0300

    Created the footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Header from './components/header/header';
 import Body from './components/body/body';
+import Footer from './components/footer/footer'
 
 function App() {
     return (
       <div className='bg-dark text-light'>
         <Header />
         <Body />
+        <Footer />
       </div>
     );
   };
